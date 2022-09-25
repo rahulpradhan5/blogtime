117d4050c3a6107d054ccbd2ca151f4ae1d0e4e5
Rahul Pradhan <97120425+rahulpradhan5@users.noreply.github.com>
2022-09-25 11:21:07 +0530

    update conn

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.mongoDB,{
+const mongoDB = "mongodb+srv://rahul:rahul2003@cluster0.jytebtf.mongodb.net/videoblog?retryWrites=true&w=majority"
+mongoose.connect(mongoDB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
